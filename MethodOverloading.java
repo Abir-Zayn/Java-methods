b389160ffe34f4f26e204d8b203024ed342e35ef
Abir-Zayn <62856303+Abir-Zayn@users.noreply.github.com>
2021-08-29 12:09:47 +0600

    Create MethodOverloading.java

---
+++ MethodOverloading.java
@@ -0,0 +1,24 @@
+public class MethodOverloading
+{
+    public static void main(String[] args) {
+    
+        int firstnum=10;
+        int secondnum=12;
+        int thirdnum=16;
+        int result,result1,result2;
+        result= maxof(firstnum, secondnum);
+        System.out.println(result);
+        
+        result1= maxof(firstnum, secondnum, thirdnum);
+        System.out.println(result1);
+        
+    }
+        static int maxof(int a, int b){
+            return (a>b)? a:b;
+        }
+        static int maxof(int a,int b,int c){
+          // condition checking => if a is greater than b then check a is greater than c if yes then  a 
+          //check if b is greater than c if yes then return b.
+            return (a>b) ? ((a>c) ? a:c) :(b>c? b:c);
+        }
+}
